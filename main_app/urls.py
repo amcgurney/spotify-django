ea9a3fed7467359b61e78d6ff250e4f9268eb9a2
Alan McGurney <mcgurneyalan@gmail.com>
2022-02-08 15:07:31 -0500

    new repo

---
+++ main_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.Home.as_view(), name="home"),
+    path('about/', views.About.as_view(), name="about"),
+    path('artists/', views.ArtistList.as_view(), name="artist_list"),
+    # Here is our new route
+    path('artists/new/', views.ArtistCreate.as_view(), name="artist_create")
+]
+
